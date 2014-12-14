8421d69a8372ede6e25dd8d8ebf1164d9914661f
Rob Prouse <rob@prouse.org>
2014-12-14 16:31:06 -0500

    Testing nunit/nunitv2#15 async support in .NET 4.0 using the async support pack

---
+++ nunit-v2/AsyncTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nunit_v2
+{
+    [TestFixture]
+    public class AsyncTests
+    {
+        [Test]
+        [Description("Testing nunit/nunitv2#15 async support in .NET 4.0 using the async support pack")]
+        public void AsyncTest()
+        {
+            Assert.That(async () => await AsyncReturnOne(), Is.EqualTo(1));
+        }
+
+        private static Task<int> AsyncReturnOne()
+        {
+            return TaskEx.Run(() => 1);
+        }
+    }
+}
