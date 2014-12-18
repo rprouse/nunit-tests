59d440f33592eabd9037e0fb9a2b2d971cfeb97b
Rob Prouse <rob@prouse.org>
2014-12-18 11:17:43 -0500

    Unit tests for issue 71 and 210

---
+++ nunit-v3/Issue210/RunsInAppDomain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace nunit.v3.Issue210
+{
+    internal class RunsInAppDomain : MarshalByRefObject
+    {
+        public void WriteToConsole()
+        {
+            Console.WriteLine("RunsInAppDomain.WriteToConsole");
+        }
+
+        public void WriteToTestContext()
+        {
+            TestContext.WriteLine( "RunsInAppDomain.WriteToTestContext" );
+        }
+    }
+}
