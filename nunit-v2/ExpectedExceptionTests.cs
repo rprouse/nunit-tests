ecc73460c0503a0cd7a6bb49722d68e4784adf73
Rob Prouse <rob@prouse.org>
2015-01-05 12:42:32 -0500

    v2 Assert.Throws example

---
+++ nunit-v2/ExpectedExceptionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace nunit.v2
+{
+    [TestFixture]
+    public class ExpectedExceptionTests
+    {
+        [Test]
+        public void ThrowsException()
+        {
+            Assert.Throws<ArgumentException>( MethodThatThrows );
+        }
+
+        private void MethodThatThrows()
+        {
+            throw new ArgumentException();
+        }
+    }
+}
