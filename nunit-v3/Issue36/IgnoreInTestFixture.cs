f0cb23635abaf0dfb03bd00d62a79c3c3815fa52
Rob Prouse <rob@prouse.org>
2015-02-06 17:43:00 -0500

    Expected exception

--- nunit-v3/Issue36/IgnoreInTestFixture.cs
+++ nunit-v3/Issue36/IgnoreInTestFixture.cs
@@ -2,7 +2,7 @@
 
 namespace nunit.v3.Issue36
 {
-    [TestFixture(Ignore = true)]
+    [TestFixture]
     [Description("This is a test to see if the v2 Issue #36 occurs in v3, https://github.com/nunit/nunitv2/issues/36")]
     public class IgnoreInTestFixture
     {
--- nunit-v3/Issue428/ExpectedException.cs
+++ nunit-v3/Issue428/ExpectedException.cs
@@ -22,7 +22,6 @@
 // ***********************************************************************
 
 using System;
-using System.Threading;
 using NUnit.Framework.Internal;
 using NUnit.Framework.Internal.Commands;
 using NUnit.Framework.Interfaces;
@@ -33,194 +32,56 @@ namespace NUnit.Framework
     /// ExpectedExceptionAttribute
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public class ExpectedExceptionAttribute : NUnitAttribute, ICommandDecorator
+    public class ExpectedExceptionAttribute : NUnitAttribute, IWrapTestMethod
     {
-        private ExpectedExceptionData _exceptionData = new ExpectedExceptionData();
+        private readonly Type _expectedExceptionType;
 
-        /// <summary>
-        /// Constructor for a given type of exception
-        /// </summary>
-        /// <param name="exceptionType">The type of the expected exception</param>
-        public ExpectedExceptionAttribute(Type exceptionType)
+        public ExpectedExceptionAttribute(Type type)
         {
-            _exceptionData.ExpectedExceptionType = exceptionType;
+            _expectedExceptionType = type;
         }
 
-        /// <summary>
-        /// Gets or sets the expected exception type
-        /// </summary>
-        public Type ExpectedException
+        public TestCommand Wrap(TestCommand command)
         {
-            get { return _exceptionData.ExpectedExceptionType; }
-            set { _exceptionData.ExpectedExceptionType = value; }
+            return new ExpectedExceptionCommand(command, _expectedExceptionType);
         }
-
-        /// <summary>
-        /// Gets or sets the user message displayed in case of failure
-        /// </summary>
-        public string UserMessage
-        {
-            get { return _exceptionData.UserMessage; }
-            set { _exceptionData.UserMessage = value; }
-        }
-
-        #region ICommandDecorator Members
-
-        TestCommand ICommandDecorator.Decorate(TestCommand command)
-        {
-            return new ExpectedExceptionCommand(command, _exceptionData);
-        }
-
-        #endregion
     }
 
     public class ExpectedExceptionCommand : DelegatingTestCommand
     {
-        private ExpectedExceptionData _exceptionData;
+        private readonly Type _expectedType;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ExpectedExceptionCommand"/> class.
-        /// </summary>
-        /// <param name="innerCommand">The inner command.</param>
-        /// <param name="exceptionData">The exception data.</param>
-        public ExpectedExceptionCommand(TestCommand innerCommand, ExpectedExceptionData exceptionData)
+        public ExpectedExceptionCommand(TestCommand innerCommand, Type expectedType)
             : base(innerCommand)
         {
-            _exceptionData = exceptionData;
+            _expectedType = expectedType;
         }
-
-
-        /// <summary>
-        /// Runs the test, saving a TestResult in the supplied TestExecutionContext
-        /// </summary>
-        /// <param name="context">The context in which the test is to be run.</param>
-        /// <returns>A TestResult</returns>
+        
         public override TestResult Execute(TestExecutionContext context)
         {
+            Type caughtType = null;
+
             try
             {
-                context.CurrentResult = innerCommand.Execute(context);
-
-                if (context.CurrentResult.ResultState == ResultState.Success)
-                    ProcessNoException(context);
+                innerCommand.Execute(context);
             }
             catch (Exception ex)
             {
-                if (ex is ThreadAbortException)
-                    Thread.ResetAbort();
-
-                ProcessException(ex, context);
+                if (ex is NUnitException)
+                    ex = ex.InnerException;
+                caughtType = ex.GetType();
             }
+
+            if (caughtType == _expectedType)
+                context.CurrentResult.SetResult(ResultState.Success);
+            else if (caughtType != null)
+                context.CurrentResult.SetResult(ResultState.Failure,
+                    string.Format("Expected {0} but got {1}", _expectedType.Name, caughtType.Name));
+            else
+                context.CurrentResult.SetResult(ResultState.Failure,
+                    string.Format("Expected {0} but no exception was thrown", _expectedType.Name));
 
             return context.CurrentResult;
         }
-
-        /// <summary>
-        /// Handles processing when no exception was thrown.
-        /// </summary>
-        /// <param name="context">The execution context.</param>
-        private void ProcessNoException(TestExecutionContext context)
-        {
-            context.CurrentResult.SetResult(ResultState.Failure, NoExceptionMessage());
-        }
-
-        /// <summary>
-        /// Handles processing when an exception was thrown.
-        /// </summary>
-        /// <param name="exception">The exception.</param>
-        /// <param name="context">The execution context.</param>
-        private void ProcessException(Exception exception, TestExecutionContext context)
-        {
-            if (exception is NUnitException)
-                exception = exception.InnerException;
-
-            if (IsExpectedExceptionType(exception))
-            {
-                context.CurrentResult.SetResult(ResultState.Success);
-            }
-            else
-            {
-                context.CurrentResult.RecordException(exception);
-
-                // If it shows as an error, change it to a failure due to the wrong type
-                if (context.CurrentResult.ResultState == ResultState.Error)
-                    context.CurrentResult.SetResult(ResultState.Failure, WrongTypeMessage(exception), ExceptionHelper.GetStackTrace(exception));
-            }
-        }
-
-        #region Helper Methods
-
-        private bool IsExpectedExceptionType(Exception exception)
-        {
-            return _exceptionData.ExpectedExceptionName == null ||
-                _exceptionData.ExpectedExceptionName.Equals(exception.GetType().FullName);
-        }
-
-        private string NoExceptionMessage()
-        {
-            string expectedType = _exceptionData.ExpectedExceptionName ?? "An Exception";
-            return CombineWithUserMessage(expectedType + " was expected");
-        }
-
-        private string WrongTypeMessage(Exception exception)
-        {
-            return CombineWithUserMessage(
-                "An unexpected exception type was thrown" + Env.NewLine +
-                "Expected: " + _exceptionData.ExpectedExceptionName + Env.NewLine +
-                " but was: " + exception.GetType().FullName + " : " + exception.Message);
-        }
-
-        private string CombineWithUserMessage(string message)
-        {
-            if (_exceptionData.UserMessage == null)
-                return message;
-            return _exceptionData.UserMessage + Env.NewLine + message;
-        }
-
-        #endregion
-    }
-
-    /// <summary>
-    /// ExpectedExceptionData is a struct used within the framework
-    /// to encapsulate information about an expected exception.
-    /// </summary>
-    public struct ExpectedExceptionData
-    {
-        #region Fields
-
-        private Type _expectedExceptionType;
-        private string _expectedExceptionName;
-
-        #endregion
-
-        #region Properties
-
-        /// <summary>
-        /// The Type of any exception that is expected.
-        /// </summary>
-        public Type ExpectedExceptionType
-        {
-            get { return _expectedExceptionType; }
-            set
-            {
-                _expectedExceptionType = value;
-                _expectedExceptionName = value.FullName;
-            }
-        }
-
-        /// <summary>
-        /// The FullName of any exception that is expected
-        /// </summary>
-        public string ExpectedExceptionName
-        {
-            get { return _expectedExceptionName; }
-        }
-
-        /// <summary>
-        /// A user message to be issued in case of error
-        /// </summary>
-        public string UserMessage { get; set; }
-
-        #endregion
     }
 }
