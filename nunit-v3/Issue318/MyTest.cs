b30d9a4d49ca0dc31e21fd1dbd1ef54f16e7540e
Rob Prouse <rob@prouse.org>
2015-03-16 22:56:52 -0400

    Refactored the unit tests for issue 318

---
+++ nunit-v3/Issue318/MyTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace nunit.v3.Issue318
+{
+    public class OnTestFixtureAttribute : TestActionAttribute
+    {
+        public override void BeforeTest(ITest test)
+        {
+            Assert.That(test, Is.Not.Null);
+            Assert.That(test.Fixture, Is.Not.Null);
+            Assert.That(test.Fixture.GetType(), Is.EqualTo(test.FixtureType));
+        }
+    }
+
+    public class OnTestMethodAttribute : TestActionAttribute
+    {
+        public override void BeforeTest(ITest test)
+        {
+            Assert.That(test, Is.Not.Null);
+            Assert.That(test.Fixture, Is.Not.Null);
+            Assert.That(test.Fixture.GetType(), Is.EqualTo(test.FixtureType));
+        }
+    }
+
+    [OnTestFixture]
+    [TestFixture]
+    public class FixtureIsNotNullForTests
+    {
+        [OnTestMethod]
+        [Test]
+        public void TestMethod()
+        {
+        }
+    }
+}
