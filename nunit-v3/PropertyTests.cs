2f344299a51625fdaf8e8d078123674de77b326c
Rob Prouse <rob@prouse.org>
2015-07-02 09:02:01 -0400

    Added a test of the CategoryAttribute as a demo for the mailing list

---
+++ nunit-v3/PropertyTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+
+namespace nunit.v3
+{
+    [TestFixture]
+    public class PropertyTests
+    {
+        [Test]
+        [Category("PDF")]
+        [Category("DOC")]
+        public void CanGetCategory()
+        {
+            IPropertyBag properties = TestContext.CurrentContext.Test.Properties;
+            var cat = properties["Category"];
+            Assert.That(cat, Contains.Item("PDF"));
+            Assert.That(cat, Contains.Item("DOC"));
+        }
+    }
+}
