6a270c5d1bf57cdbf98786276e5d750044625b27
Rob Prouse <rob@prouse.org>
2015-09-29 10:30:41 -0400

    Added NUnit 3 test for issue 838

---
+++ nunit-v3/Issue838/TestCustomPropertyAttribute.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunit.v3.Issue838
+{
+    [TestFixture]
+    public class TestCustomPropertyAttribute
+    {
+        [Test]
+        [CustomProperty(CustomPropertyType.First)]
+        public void MinimumWorkingTest()
+        {
+            Assert.Pass("This passes!");
+        }
+    }
+    public enum CustomPropertyType
+    {
+        First,
+        Second
+    }
+
+    public class CustomPropertyAttribute : PropertyAttribute
+    {
+        public CustomPropertyAttribute(CustomPropertyType type)
+            : base(type)
+        {
+        }
+    }
+}
