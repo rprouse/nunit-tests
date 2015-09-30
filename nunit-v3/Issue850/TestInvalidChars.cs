cb75c20f4a7a020ebc2e047b2322009d8f88eea3
Rob Prouse <rob@prouse.org>
2015-09-30 09:59:18 -0400

    Added a test to confirm issue 850

---
+++ nunit-v3/Issue850/TestInvalidChars.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+
+namespace nunit.v3.Issue850
+{
+    [TestFixture]
+    public class TestInvalidChars
+    {
+        public static IEnumerable<char> InvalidCharacters => Path.GetInvalidFileNameChars();
+
+        // This causes an exception in NUnit 3 beta 4
+        //[Test]
+        //public void HandleInvalidCharacters([ValueSource("InvalidCharacters")] char invalidCharacter)
+        //{
+        //}
+    }
+}
