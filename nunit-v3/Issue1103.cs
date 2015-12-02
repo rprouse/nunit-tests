e593d82a182982055ec87518b902048b101d900f
Rob Prouse <rob@prouse.org>
2015-12-01 21:02:45 -0500

    Test for issue 1103

---
+++ nunit-v3/Issue1103.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunit.v3
+{
+    public class BaseClass1103
+    {
+        public static readonly object[] BaseTestCaseSource =
+        {
+             new TestCaseData("testCase")
+        };
+    }
+
+    [TestFixture]
+    public class ChildClass1103 : BaseClass1103
+    {
+
+        [TestCaseSource(typeof(BaseClass1103), "BaseTestCaseSource")]
+        public void ChildTest(string argument)
+        {
+            Assert.That(argument, Is.Not.Null);
+        }
+    }
+}
