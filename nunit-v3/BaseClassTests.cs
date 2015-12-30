af0e6c95391b9e41f493ce7d283e11eef58b0bf0
Rob Prouse <rob@prouse.org>
2015-12-30 09:27:55 -0500

    Testing logging after each test runs.

--- nunit-v3/BaseClassTests.cs
+++ nunit-v3/BaseClassTests.cs
@@ -7,21 +7,32 @@ using System.Threading.Tasks;
 
 namespace nunit.v3
 {
-    [TestFixture]
-    public class BaseClassTests
+    public class LoggingBase
     {
-        [Test]
-        public virtual void TestMethod()
+        [TearDown]
+        public void LogResults()
         {
-            Assert.Pass(nameof(BaseClassTests));
+            TestContext.WriteLine($"{TestContext.CurrentContext.Test.Name}: {TestContext.CurrentContext.Result.Outcome}");
         }
     }
 
     [TestFixture]
-    public class DerivedClassTests : BaseClassTests
+    public class DerivedClassTests : LoggingBase
     {
+        [SetUp]
+        public void SetUp()
+        {
+            TestContext.WriteLine("SetUp");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            TestContext.WriteLine("SetUp");
+        }
+
         [Test]
-        public override void TestMethod()
+        public void TestMethod()
         {
             Assert.Pass(nameof(DerivedClassTests));
         }
