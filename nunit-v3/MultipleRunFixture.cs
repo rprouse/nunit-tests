70d6ef9c8e61f2bb3044bd3a8200c065461f7b50
Rob Prouse <rob@prouse.org>
2016-01-29 12:25:48 -0500

    Example code for a StackOverflow question

---
+++ nunit-v3/MultipleRunFixture.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace nunit.v3
+{
+    [TestFixtureSource("TestData")]
+    public class MultipleRunFixture
+    {
+        int _counter;
+
+        public MultipleRunFixture(int counter)
+        {
+            _counter = counter;
+        }
+
+        public static IEnumerable<int> TestData =>
+            Enumerable.Range(0, 100);
+
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass($"Test run {_counter}");
+        }
+    }
+}
