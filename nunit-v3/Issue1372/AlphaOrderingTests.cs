71bf87c7c03b6b650b063c29289e736f53acde39
Rob Prouse <rob@prouse.org>
2016-04-14 17:54:05 -0400

    Testing issue 1372

---
+++ nunit-v3/Issue1372/AlphaOrderingTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunit.v3.Issue1372
+{
+    [TestFixture]
+    public class AlphaOrderingTests
+    {
+        [Test]
+        public void A_Test()
+        {
+            TestContext.WriteLine("A");
+        }
+
+        [Test]
+        public void D_Test()
+        {
+            TestContext.WriteLine("D");
+        }
+
+        [Test]
+        public void E_Test()
+        {
+            TestContext.WriteLine("E");
+        }
+
+        [Test]
+        public void B_Test()
+        {
+            TestContext.WriteLine("B");
+        }
+
+        [Test]
+        public void C_Test()
+        {
+            TestContext.WriteLine("C");
+        }
+
+        [Test]
+        public void F_Test()
+        {
+            TestContext.WriteLine("F");
+        }
+    }
+}
