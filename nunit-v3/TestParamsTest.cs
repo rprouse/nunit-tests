436353cfada26120ef2f2f7816f02f05f4c0a13a
Rob Prouse <rob@prouse.org>
2016-07-13 10:36:35 -0400

    Updated to NUnit 3.4.1 and test the new --params command line

---
+++ nunit-v3/TestParamsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunit.v3
+{
+    [TestFixture]
+    public class TestParamsTest
+    {
+        [Test]
+        public void TestCommandLineParameters()
+        {
+            var code = TestContext.Parameters.Get("Code", "<unknown>");
+            var date = TestContext.Parameters.Get("Date", DateTime.MinValue);
+
+            TestContext.WriteLine($"Fetched test parameters {code} and {date}");
+        }
+    }
+}
