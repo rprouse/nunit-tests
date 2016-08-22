27ef7d818b6377d7b3e0010c0a57965aadf0bc78
Rob Prouse <rob@prouse.org>
2016-08-22 11:24:56 -0400

    Combinatorial test

---
+++ nunit-v3/CombinatorialTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace nunit.v3
+{
+    [TestFixture]
+    public class CombinatorialTests
+    {
+        [Combinatorial]
+        public void SomeTest([Values] bool flag, [Values(2, 5)] int someValue)
+        {
+            TestContext.WriteLine($"{flag} - ${someValue}");
+        }
+    }
+}
