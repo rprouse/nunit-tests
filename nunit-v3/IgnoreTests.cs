b917b4ec8b4a0ac30fcdaeac7255b3e6f213667b
Rob Prouse <rob@prouse.org>
2016-08-26 09:17:03 -0400

    Issue 1762

---
+++ nunit-v3/IgnoreTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunit.v3
+{
+[TestFixture]
+public class IgnoreTests
+{
+    [Ignore("NUnit Issue #1762")]
+    [Test]
+    public void IgnoreBeforeTest()
+    {
+        Assert.Fail("This test should be ignored");
+    }
+
+    [Test]
+    [Ignore("NUnit Issue #1762")]
+    public void IgnoreAfterTest()
+    {
+        Assert.Fail("This test should be ignored");
+    }
+
+    [Ignore("NUnit Issue #1762")]
+    [TestCase(1)]
+    public void IgnoreBeforeTestCase(int i)
+    {
+        Assert.Fail("This test should be ignored");
+    }
+
+    [TestCase(1)]
+    [Ignore("NUnit Issue #1762")]
+    public void IgnoreAfterTestCase(int i)
+    {
+        Assert.Fail("This test should be ignored");
+    }
+}
+}
