924bedda7f4156d89b5fe83e0f143d954618ef33
Rob Prouse <rob@prouse.org>
2016-10-18 14:38:43 -0400

    TestCaseSource with one element test

--- nunit-v3/TestCaseDataTests.cs
+++ nunit-v3/TestCaseDataTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,20 @@ namespace nunit.v3
         public void TestStringArrayArguments(string[] sizes)
         {
             Assert.That(sizes.Length, Is.GreaterThan(1));
+        }
+
+        private static IEnumerable _byteArrays = new object[]
+        {
+            new TestCaseData(new byte[] { 80 }), // - works fine with commented out
+            new byte[] { 80, 75 },
+            new byte[] { 80, 75, 03 }
+        };
+
+        [Test]
+        [TestCaseSource("_byteArrays")]
+        public void TestStuff(byte[] byteArray)
+        {
+            Assert.IsFalse(false);
         }
     }
 }
