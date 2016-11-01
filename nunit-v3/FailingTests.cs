fa8e0634caed5e21c2236850b7d333a65b87d38d
Rob Prouse <rob@prouse.org>
2016-10-31 22:35:27 -0400

    Confirming nunit/dotnet-test-nunit#60

---
+++ nunit-v3/FailingTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace nunit.v3
+{
+    [Ignore("These are failing tests")]
+    public class FailingTest
+    {
+        [Test]
+        public void WatchMeFail()
+        {
+            Assert.Fail("Watch me fail!");
+        }
+
+        [Test]
+        public void WatchMeThrowAnException()
+        {
+            throw new NotImplementedException("Watch me crash and burn!");
+        }
+
+        [Test]
+        public void TestSuperHero()
+        {
+            var obj = new SuperHero();
+            obj.Batman();
+            Assert.Pass();
+        }
+    }
+
+    public class SuperHero
+    {
+        public void Batman()
+        {
+            throw new Exception("Batman failed");
+        }
+    }
+}
