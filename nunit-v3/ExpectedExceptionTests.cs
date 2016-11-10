8afaabb96eb4eb612b23e8c0197a920611965240
Rob Prouse <rob@prouse.org>
2016-11-10 08:15:18 -0800

    Async tests

--- nunit-v3/ExpectedExceptionTests.cs
+++ nunit-v3/ExpectedExceptionTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.Threading;
 
 namespace nunit.v3
 {
@@ -13,8 +14,8 @@ namespace nunit.v3
         [Test]
         public void ThrowsException()
         {
-            Assert.Throws<ArgumentException>( MethodThatThrows );
-            Assert.That( MethodThatThrows, Throws.ArgumentException );
+            Assert.Throws<ArgumentException>(MethodThatThrows);
+            Assert.That(MethodThatThrows, Throws.ArgumentException);
         }
 
         private void MethodThatThrows()
@@ -22,15 +23,15 @@ namespace nunit.v3
             throw new ArgumentException();
         }
 
-        [TestCase( 1 )]
-        public void TestThrows( int x )
+        [TestCase(1)]
+        public void TestThrows(int x)
         {
-            MustBeGreaterThanZero( x );
+            MustBeGreaterThanZero(x);
         }
 
-        private void MustBeGreaterThanZero( int x )
+        private void MustBeGreaterThanZero(int x)
         {
-            if ( x <= 0 ) throw new ArgumentException( "Must be greater than 0", "x" );
+            if (x <= 0) throw new ArgumentException("Must be greater than 0", "x");
         }
 
 
@@ -45,15 +46,51 @@ namespace nunit.v3
             throw new Exception();
         }
 
-    [TestCase("27/04/2025", "Holiday cannot start or end on a weekend or non-working day")]
-    public void AddHolidays_Exceptions(string date, string expectedMessage)
-    {
-        Assert.That(() => ParseDate(date), Throws.ArgumentException.With.Message.EqualTo(expectedMessage));
-    }
+        [TestCase("27/04/2025", "Holiday cannot start or end on a weekend or non-working day")]
+        public void AddHolidays_Exceptions(string date, string expectedMessage)
+        {
+            Assert.That(() => ParseDate(date), Throws.ArgumentException.With.Message.EqualTo(expectedMessage));
+        }
 
         void ParseDate(string date)
         {
             throw new ArgumentException("Holiday cannot start or end on a weekend or non-working day");
+        }
+
+        [Test]
+        public void Test_Exception()
+        {
+            Assert.That(() => { throw new Exception("KAPOW"); }, Throws.Exception);
+        }
+
+        [Test]
+        public void Test_Blocking()
+        {
+            Assert.That(() => { Thread.Sleep(100); throw new Exception("KAPOW"); }, Throws.Exception);
+        }
+
+        //[Test]
+        //public async Task Test_Async_1()
+        //{
+        //    Assert.That(async () => { await Task.Delay(100); throw new Exception("KAPOW"); }, Throws.Exception);
+        //}
+
+        [Test]
+        public void Test_Async_4()
+        {
+            Assert.That(async () => { await Task.Delay(100); throw new ArgumentException("KAPOW"); }, Throws.InstanceOf<Exception>());
+        }
+
+        [Test]
+        public void Test_Async_3()
+        {
+            Assert.That(async () => { await Task.Delay(100); throw new Exception("KAPOW"); }, Throws.InstanceOf<Exception>());
+        }
+
+        [Test]
+        public void Test_Async_2()
+        {
+            Assert.That(async () => { await Task.Delay(100); throw new Exception("KAPOW"); }, Throws.Exception.TypeOf(typeof(Exception)));
         }
     }
 }
