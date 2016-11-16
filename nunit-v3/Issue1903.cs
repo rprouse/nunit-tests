754f94b4cc009fed89bb6c185db261b2d9b54e72
Rob Prouse <rob@prouse.org>
2016-11-16 12:12:12 -0500

    Tests for issue 1903

---
+++ nunit-v3/Issue1903.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections;
+
+namespace nunit.v3
+{
+    [TestFixture]
+    public class Issue1903
+    {
+        [TestCaseSource(nameof(TestData))]
+        public void TestExpectedLambda(int x, Func<int, bool> expected)
+        {
+            Assert.That(expected(x), Is.True);
+        }
+
+        static IEnumerable TestData()
+        {
+            yield return new TestCaseData(10, new Func<int, bool>(x => x > 5));
+            yield return new TestCaseData(2, new Func<int, bool>(x => x < 3));
+        }
+    }
+}
