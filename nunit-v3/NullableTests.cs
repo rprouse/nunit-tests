e043e8386ee53a420432be14bd0b56ce42b63b41
Rob Prouse <rob@prouse.org>
2016-11-24 10:49:52 -0500

    Nullable boolean tests

---
+++ nunit-v3/NullableTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nunit.v3
+{
+    [TestFixture]
+    public class NullableTests
+    {
+        [Test]
+        public void TestNullableBooleanTrue()
+        {
+            bool? b = true;
+            Assert.That(b, Is.True);
+        }
+
+        [Test]
+        public void TestNullableBooleanFalse()
+        {
+            bool? b = false;
+            Assert.That(b, Is.False);
+        }
+
+        [Test]
+        public void TestNullableBooleanNull()
+        {
+            bool? b = null;
+            Assert.That(b, Is.Null);
+        }
+    }
+}
