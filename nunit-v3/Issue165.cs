14c4080245e3021a3f59f35c77fd6a6990c316e1
Rob Prouse <rob.prouse@bentley.com>
2018-01-23 08:41:39 -0500

    Added tests for adapter issue 165

---
+++ nunit-v3/Issue165.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace nunit.v3
+{
+    public class Issue165
+    {
+        [TestCase("Hello")]
+        [TestCase("\u0002")]
+        public void DoesntStart(string aString)
+        {
+            Assert.Pass($"Ran Doesn't Start for {aString}");
+        }
+    }
+}
