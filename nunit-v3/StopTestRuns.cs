325a02be08ed90b640a3b5bc28f5f8cb0cbc38fd
Rob Prouse <rob.prouse@bentley.com>
2018-04-13 14:58:16 -0400

    Example of how to stop test runs on the first failure.

---
+++ nunit-v3/StopTestRuns.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace nunit.v3
+{
+    [TestFixture]
+    public class StopTestRuns
+    {
+        // Switch back to false to test this behaviour
+        bool _stopTests = true;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Assume.That(_stopTests, Is.False);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if(TestContext.CurrentContext.Result.Outcome.Status != NUnit.Framework.Interfaces.TestStatus.Passed)
+            {
+                _stopTests = true;
+            }
+        }
+
+        [Test]
+        public void TestOne()
+        {
+            Assert.Pass("This passed");
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            Assert.Fail("This failed too");
+        }
+
+        [Test]
+        public void TestThree()
+        {
+            Assert.Fail("This failed three");
+        }
+    }
+}
