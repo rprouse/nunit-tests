be6af95fb77c2e8805c2453a0f88e75abdff38ec
Rob Prouse <rob@prouse.org>
2019-06-11 09:25:31 -0400

    Playing with the NUnitLite assembly

--- nunitlite-v3/Program.cs
+++ nunitlite-v3/Program.cs
@@ -8,10 +8,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -21,6 +21,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ***********************************************************************
 
+using System.Reflection;
 using NUnitLite;
 
 namespace NUnitLite.Tests
@@ -35,7 +36,9 @@ namespace NUnitLite.Tests
         /// <param name="args"></param>
         public static int Main(string[] args)
         {
-            return new AutoRun().Execute(args);
+            var asm = Assembly.GetExecutingAssembly();
+
+            return new AutoRun(asm).Execute(args);
         }
     }
 }
