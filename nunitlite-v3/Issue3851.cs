4e4e3eeee932a7d5c81547efedd984f3644b5bb9
Rob Prouse <rob@prouse.org>
2021-11-14 16:02:48 -0500

    Issue 3851

---
+++ nunitlite-v3/Issue3851.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nunit.v3
+{
+    class Issue3851
+    {
+        //public extern int ExternGetOnly { get; }
+        //public extern int ExternSetOnly { set; }
+        //public extern int ExternProperty { get; set; }
+
+        //public extern void ExternMethod();
+    }
+}
