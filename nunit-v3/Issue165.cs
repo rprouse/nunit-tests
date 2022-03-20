dbd4a28a0466ec79f30d7408e124e59eef7c82ed
Rob Prouse <rob@prouse.org>
2022-03-20 14:21:50 -0400

    Testing NUnit 3.13.3 release

--- nunit-v3/Issue165.cs
+++ nunit-v3/Issue165.cs
@@ -1,11 +1,11 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 namespace nunit.v3
 {
     public class Issue165
     {
         [TestCase("Hello")]
-        [TestCase("\u0002")]
+        //[TestCase("\u0002")]
         public void DoesntStart(string aString)
         {
             Assert.Pass($"Ran Doesn't Start for {aString}");
